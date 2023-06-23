463d5e4b708d951f6cc02012cc29eb5a303e96d3
RenaProsto <renaprosto123@gmail.com>
2023-06-23 20:56:29 +0500

    первый коммит

---
+++ src/main/java/com/example/hwcalculator1/HwCalculator1Application.java
@@ -0,0 +1,13 @@
+package com.example.hwcalculator1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwCalculator1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HwCalculator1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hwcalculator1/HwCalculator1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hwcalculator1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwCalculator1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
